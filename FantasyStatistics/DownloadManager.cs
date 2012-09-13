96a4ef15974def2e776a144582798e84cef1640b
Andrew Afanasiev <afanasiev.andrew@gmail.com>
2012-09-13 16:12:16 +0600

    add SelectTopPlayers into class Manager

--- FantasyStatistics/DownloadManager.cs
+++ FantasyStatistics/DownloadManager.cs
@@ -4,6 +4,9 @@ using System.Threading;
 using HtmlAgilityPack;
 using System.IO;
 using System.Text;
+using IniParser;
+using System.Data.SQLite;
+using System.Data.Common;
 
 namespace FantasyStatistics
 {
@@ -11,9 +14,65 @@ namespace FantasyStatistics
     {
         private LinkTour linkTour;
 
-        public DownloadManager(LinkTour _linkTour)
+        private DownloadManager(LinkTour _linkTour)
         {
             linkTour = new LinkTour(_linkTour.link, _linkTour.tour, _linkTour.countPage, _linkTour.dbPath);
+        }
+
+        public static DownloadManager ReadIniFile(String _path)
+        {
+            String link = "", dbPath = "";
+            int tour = 0, countPage  = 0;
+            FileIniDataParser parser = new FileIniDataParser();
+
+            IniData parserData = parser.LoadFile(_path);
+
+            foreach (SectionData section in parserData.Sections)
+            {
+                foreach (KeyData kvp in section.Keys)
+                {
+                    switch(kvp.KeyName)
+                    {
+                        case "link":
+                            link = kvp.Value;
+                            break;
+                        case "dbPath":
+                            dbPath = kvp.Value;
+                            break;
+                        case "tour":
+                            tour = Convert.ToInt32(kvp.Value);
+                            break;
+                        case "countPage":
+                            countPage = Convert.ToInt32(kvp.Value);
+                            break;
+                    }
+                }
+            }
+
+            return new DownloadManager(new LinkTour(link, tour, countPage, dbPath));
+        }
+
+        public void SelectTopPlayers(int _startIndex, int _endIndex)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(string.Format("Data Source={0};", linkTour.dbPath)))
+            {
+                connection.Open();
+
+                using (SQLiteCommand sqlSel = new SQLiteCommand("select us.name, p2.counts-p1.counts, us.link from points p1, points p2, users us where p1.id=p2.id and p1.tour=@endIndex and p2.tour=@startIndex and p1.id=us.id order by p2.counts-p1.counts desc limit 40", connection))
+                {
+                    sqlSel.Parameters.Add(new SQLiteParameter("@startIndex", _startIndex));
+                    sqlSel.Parameters.Add(new SQLiteParameter("@endIndex", _endIndex));
+
+                    SQLiteDataReader reader = sqlSel.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}  |  {1}  |  {2}", reader[0].ToString(), reader[1].ToString(), reader[2].ToString());
+                    }
+                }
+
+                connection.Close();
+            }
         }
 
         public void Start()
---
+++ FantasyStatistics/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading;
+using HtmlAgilityPack;
+using System.Data.SQLite;
+using System.Data.Common;
+
+namespace FantasyStatistics
+{
+    class Program
+    {
+        private static void UpdateDB()
+        {
+            String path = "ConfigurationFile.ini";
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File {0} not found", path);
+                Console.ReadLine();
+                return;
+            }
+
+            DownloadManager dw = DownloadManager.ReadIniFile(path);
+            dw.Start();
+
+            Console.ReadLine();
+        }
+   
+        static void Main(string[] args)
+        {
+            if (args.Length.Equals(0))
+            {
+                UpdateDB();
+
+                return;
+            }
+
+            foreach (var arg in args)
+            {
+                if (arg.Equals("-updateDB"))
+                {
+                    UpdateDB();
+
+                    continue;
+                }
+
+                if (arg.Substring(0, 12).Equals("-topPlayers:"))
+                {
+                    String str = arg.Remove(0, 12);
+
+                    int secondTour = Convert.ToInt32(str.Substring(0, str.IndexOf('-')));
+                    int firstTour = Convert.ToInt32(str.Substring(str.IndexOf('-') + 1, str.Length - str.IndexOf('-') - 1));
+
+                    String path = "ConfigurationFile.ini";
+
+                    if (!File.Exists(path))
+                    {
+                        Console.WriteLine("File {0} not found", path);
+                        Console.ReadLine();
+                        return;
+                    }
+
+                    DownloadManager dw = DownloadManager.ReadIniFile(path);
+                    dw.SelectTopPlayers(secondTour, firstTour);
+
+                    continue;
+                }
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
